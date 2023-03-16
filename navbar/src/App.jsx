188b2311aab12ccd2bd1b40dc5f0179f6a8a1ee4
Bill Tsay <billtsay@gmail.com>
2023-03-15 23:55:03 -0500

    update navbar

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -9,22 +9,26 @@ import Explore from "./Pages/Explore";
 import Home from "./Pages/Home";
 import Settings from "./Pages/Settings";
 import Statistics from "./Pages/Statistics";
+import Topbar from './Components/Topbar';
 
 function App() {
   return (
-    <div className="App">
-      <Sidenav/>
-      <main>
-      <Routes>
-        <Route  path="/" element={<Home />}/>
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/statistics" element={<Statistics />}/>
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      </main>
-     
+    <div>
+      <header>
+        <Topbar />
+      </header>
+      <div className="App">
+        <Sidenav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 }
-
 export default App;
